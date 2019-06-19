78c4e373545c058b7151a74ca43b1c3c61319890
tsyl96 <33026840+tsyl96@users.noreply.github.com>
2019-06-19 15:25:04 +0630

    19.6.2019
    final code including cron job

--- pre_process.py
+++ pre_process.py
@@ -1,18 +1,21 @@
+
+#!/usr/bin/python3
 from selenium import webdriver
 from datetime import date
 import datetime
 import time
 import csv
 import os
+from pathlib import Path
 
 today = date.today()
 d_out = today.strftime("%d.%m.%y")
 hr = datetime.datetime.now().hour
+out_folder = Path('/home/thetsu/Desktop/autoprogram/cuic')
 #const_time = '(' + str(5) + '-' + str(6) + ')'
 #out_name = d_out + const_time + ".csv"
 display_filename = "Hourly Report for(" + d_out + ").csv"
-with open(display_filename, mode='w') as file:
+with open(str( out_folder / display_filename), mode='w') as file:
     file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     file_writer.writerow(
-        ['Interval', 'CallOffered', 'CallsHandled', 'ASA', 'AHT', 'AvgTalkTime', 'AvgHoldTime', 'AbanRate',
-         'SLPercentage','Short Call'])
+        ['Interval', 'CallOffered', 'CallsHandled', 'AbanRate', 'SLPercentage', 'ASA', 'AHT', 'AvgTalkTime', 'AvgHoldTime', 'Short Call'])
